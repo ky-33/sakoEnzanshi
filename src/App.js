8570907831a085d4f10da1b2d0ca9a801b033020
ky-33 <68935925+ky-33@users.noreply.github.com>
2023-06-14 23:34:59 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>三項演算子</h1>
+      {true ? <h1>true</h1> : <h1>false</h1>}
+    </div>
+  );
+}
